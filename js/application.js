37b8ee6fdae078ea939cbdd796a0f25a118d4ad5
Latsuj <justal.kevin@gmail.com>
2020-07-26 15:14:04 +0800

    [FEATURE] Adding the file list and the animation between category and file list

--- js/application.js
+++ js/application.js
@@ -1,5 +1,6 @@
 let github_directories;
 const category = document.querySelector('#category');
+const file = document.querySelector('#file');
 
 const fetch_datas =  async function(url) {
 	try {
@@ -31,20 +32,40 @@ const loading_file = async function(directory_name,file_name) {
 	//return directories;
 }
 
-//<i class="material-icons">chevron_right</i>
-const loading_categories = async function() {
-	github_directories = await loading_directories(); 
-	github_directories.map(x => {
-		let ul = category.querySelector('ul');
+const create_list = async function(array,list,action) {
+	array.map(x => {
 		let nodeli = document.createElement("li");
 		let nodei = document.createElement("i");
-		nodeli.setAttribute("data-category", x.name);
+		let name = x.name;
+		name = name.replace(/DEV\_|\.Md/gi,"");
+		name = name.replace(/\_/gi," ");
+		console.log(name);
+		nodeli.setAttribute("data-category", name);
 		nodei.setAttribute("class", "material-icons");
 		nodei.appendChild(document.createTextNode("chevron_right"));
-		nodeli.appendChild(document.createTextNode(x.name));
+		nodeli.appendChild(document.createTextNode(name));
 		nodeli.appendChild(nodei);
-		ul.appendChild(nodeli); 
+		nodeli.addEventListener("click", action);
+		list.appendChild(nodeli); 
 	})
+}
+
+const category_selected = async function(e) {
+    let node = e.currentTarget;
+    let github_category_selected = node.dataset.category;
+    category.classList.add("selected");
+    const github_files = await loading_files_in_directory(github_category_selected);
+	let ul = file.querySelector('ul');
+	create_list(github_files,ul,category_selected);
+	file.classList.remove("not-selected");
+    
+	console.log(github_files);
+}
+
+const loading_categories = async function() {
+	github_directories = await loading_directories(); 
+	let ul = category.querySelector('ul');
+	create_list(github_directories,ul,category_selected);
 }
 loading_categories();
 
