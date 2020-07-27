c00860405cb08464ccba9f7ad248ea4fc160fc96
Latsuj <justal.kevin@gmail.com>
2020-07-27 23:32:23 +0800

    [FIX] Fixing a bug and adding the page for the md, adding the animation

--- js/application.js
+++ js/application.js
@@ -1,7 +1,9 @@
 let github_directories;
 const category = document.querySelector('#category');
 const file = document.querySelector('#file');
+const md = document.querySelector('#md');
 const back_file_to_category = document.querySelector('#file .back');
+const back_md_to_file = document.querySelector('#md .back');
 
 const fetch_datas =  async function(url) {
 	try {
@@ -12,13 +14,13 @@ const fetch_datas =  async function(url) {
 }
 
 const loading_directories = async function() {
-	let fetching,infos,infos_directories;
-	if (localStorage.infos_directories) infos_directories = localStorage.getItem("infos_directories");
-	if(!infos_directories) {
+	let fetching,infos;
+	if(localStorage.getItem("infos_directories") === null) {
 		fetching = await fetch_datas('https://api.github.com/repos/justalk/Informations/contents/');
 		infos = await fetching.json();
 		localStorage.setItem("infos_directories", JSON.stringify(infos));
 	} else {
+		let infos_directories = localStorage.getItem("infos_directories")
 		infos = JSON.parse(infos_directories);
 	}
 	const directories = infos.filter(x => x.type=="dir" && x.name!="modules");
@@ -26,13 +28,14 @@ const loading_directories = async function() {
 }
 
 const loading_files_in_directory = async function(directory_name) {
-	let fetching,infos,infos_directory;
-	if (localStorage[directory_name]) infos_directory = localStorage.getItem(directory_name);
-	if(!infos_directory) {
+	let fetching,infos;
+	console.log(directory_name);
+	if(localStorage.getItem(directory_name) === null) {
 		fetching = await fetch_datas('https://api.github.com/repos/justalk/Informations/contents/'+directory_name);
 		infos = await fetching.json();
 		localStorage.setItem(directory_name, JSON.stringify(infos));
 	} else  {
+		let infos_directory = localStorage.getItem(directory_name)
 		infos = JSON.parse(infos_directory);
 	}
 	return infos;
@@ -43,11 +46,9 @@ const loading_file = async function(directory_name,file_name) {
 	const infos = await fetching.json();
 	return infos;
 	console.log(infos);
-	//const directories = infos.filter(x => x.type=="dir" && x.name!="modules");
-	//return directories;
 }
 
-const create_list = async function(array,list,action) {
+const create_list = async function(array,list,action,dataset) {
 	array.map(x => {
 		let nodeli = document.createElement("li");
 		let nodei = document.createElement("i");
@@ -55,7 +56,7 @@ const create_list = async function(array,list,action) {
 		name = name.replace(/DEV\_|\.Md/gi,"");
 		name = name.replace(/\_/gi," ");
 		console.log(name);
-		nodeli.setAttribute("data-category", name);
+		nodeli.setAttribute(dataset, x.name);
 		nodei.setAttribute("class", "material-icons");
 		nodei.appendChild(document.createTextNode("chevron_right"));
 		nodeli.appendChild(document.createTextNode(name));
@@ -72,16 +73,14 @@ const category_selected = async function(e) {
     const github_files = await loading_files_in_directory(github_category_selected);
 	let ul = file.querySelector('ul');
 	ul.innerHTML = '';
-	create_list(github_files,ul,category_selected);
+	create_list(github_files,ul,file_selected,"data-file");
 	file.classList.remove("not-selected");
-    
-	console.log(github_files);
 }
 
 const loading_categories = async function() {
 	github_directories = await loading_directories(); 
 	let ul = category.querySelector('ul');
-	create_list(github_directories,ul,category_selected);
+	create_list(github_directories,ul,category_selected,"data-category");
 }
 loading_categories();
 
@@ -91,6 +90,21 @@ const back_to_category = function() {
 	category.classList.remove("selected");
 }
 back_file_to_category.addEventListener("click", back_to_category);
+
+const file_selected = async function(e) {
+    let node = e.currentTarget;
+    let github_category_selected = node.dataset.category;
+    let github_file_selected = node.dataset.file;
+    file.classList.add("selected");
+    //const github_md = await loading_file(github_category_selected,github_file_selected);
+	md.classList.remove("not-selected");
+}
+
+const back_to_file = function() {
+	md.classList.add("not-selected");
+	file.classList.remove("selected");
+}
+back_md_to_file.addEventListener("click", back_to_file);
 
 /**
 const reading_github = async function() {
