e43b64288ad19496875ad7a6c548a00f3794bcc6
Latsuj <justal.kevin@gmail.com>
2020-08-10 20:58:26 +0800

    [FEATURE] Adding the key event for moving with the keys between the project

--- js/application.js
+++ js/application.js
@@ -118,4 +118,21 @@ const back_to_file = function() {
 }
 back_md_to_file.addEventListener("click", back_to_file);
 
-//reading_github();
+document.onkeydown = actions_key_press;
+
+function actions_key_press(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+    }
+}
